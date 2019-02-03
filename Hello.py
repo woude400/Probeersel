46ec1ca43567e52eca5a78194e71a09abe157141
woude400 <47241588+woude400@users.noreply.github.com>
2019-02-03 19:15:13 +0100

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,24 @@
+#Gebruik Hashtags voor comments
+print("Hello world")
+#de taal is hoofdletter gevoelig Print en print zijn 2 verschillende dingen
+#x="5"
+#y="Niek"
+#print(x)
+#print(y)
+#z=x+y
+#print z
+#optellen met letters en cijfers lukt niet
+x="Niek is "
+y="Awesome"
+z=x+y
+print(z)
+#optellen met alleen letters lukt wel
+a=2
+b=4
+c=a+b
+print(c)
+#optellen door middel van cijfers toe te kennen aan variabelen is ook mogelijk
+x=1.5 #floating nummer = met 1 decimaal
+print(type(x))
+
+
